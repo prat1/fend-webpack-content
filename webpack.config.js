e3c89c04fcb6586692ad4845a47281ae0a99457a
Prathyusha <pratgermany@gmail.com>
2021-01-29 19:23:00 +0100

    2- add-webpack-entry

--- webpack.config.js
+++ webpack.config.js
@@ -3,4 +3,13 @@ const webpack = require('webpack')
 
 module.exports = {
     entry: './src/client/index.js',
+	module: {
+		rules: [
+		{
+			test: '/\.js$/',
+			exclude: /node-modules/,
+			loaders: 'babel-loader'
+		}
+		]
+	}
 }
